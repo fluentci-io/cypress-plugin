f32b4356a61962381697b8ec479310db45316b73
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-06-14 04:06:02 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Cue Plugin
+
+[![ci](https://github.com/fluentci-io/cue-plugin/actions/workflows/ci.yml/badge.svg)](https://github.com/fluentci-io/cue-plugin/actions/workflows/ci.yml)
+
+This plugin sets up your CI/CD pipeline with [Cypress](https://www.cypress.io/).
+
+## 🚀 Usage
+
+Add the following command to your CI configuration file:
+
+```bash
+fluentci run --wasm cypress install
+```
+
+## Functions
+
+| Name    | Description                                                     |
+| ------- | --------------------------------------------------------------- |
+| run     | Runs Cypress tests from the CLI without the GUI                 |
+| install | Installs the Cypress executable matching this package's version |
+| cache   | Manages the Cypress binary cache                                |
+| info    | Prints Cypress and system information                           |
+| verify  | Verifies that Cypress is installed correctly and executable     |
+| help    | Shows CLI help and exits                                        |
+| version | prints Cypress version                                          |
+
+## Code Usage
+
+Add `fluentci-pdk` crate to your `Cargo.toml`:
+
+```toml
+[dependencies]
+fluentci-pdk = "0.1.9"
+```
+
+Use the following code to call the plugin:
+
+```rust
+use fluentci_pdk::dag;
+
+// ...
+
+dag().call("https://pkg.fluentci.io/cypress@v0.1.0?wasm=1", "setup", vec!["latest"])?;
+```
+
+## 📚 Examples
+
+Github Actions:
+
+```yaml
+- name: Setup Fluent CI CLI
+  uses: fluentci-io/setup-fluentci@v5
+  with:
+    wasm: true
+    plugin: cypress
+    args: |
+      help
+- name: Show cypress version
+  run: |
+    fluentci run --wasm cypress version
+```
---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use extism_pdk::*;
+use fluentci_pdk::dag;
+
+#[plugin_fn]
+pub fn run(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "run", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn install(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "install", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn cache(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "cache", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn info(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "info", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn verify(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "verify", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn help(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "help", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn version(args: String) -> FnResult<String> {
+    dag().call(
+        "https://pkg.fluentci.io/bun@v0.1.0?wasm=1",
+        "setup",
+        vec!["latest"],
+    )?;
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["pkgx", "bunx", "cypress", "version", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
